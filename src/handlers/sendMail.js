3c099d347e4ed8e39da41872863d602cdbefc8f0
estebanpablo89 <estebanpablo89@gmail.com>
2021-05-02 20:50:34 -0500

    test email

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,6 +1,31 @@
+import AWS from 'aws-sdk';
+
+const ses = new AWS.SES({ region: 'us-east-2' });
+
 async function sendMail(event, context) {
-  console.log(event);
-  return event;
+  const params = {
+    Source: 'estebanpablo89@gmail.com',
+    Destination: {
+      ToAddresses: ['estebanpablo89@gmail.com'],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: 'Hello from the world',
+        },
+      },
+      Subject: {
+        Data: 'Test mail',
+      },
+    },
+  };
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export const handler = sendMail;
