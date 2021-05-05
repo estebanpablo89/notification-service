57d63ab74ca55aad7cbd0e5b8f655b32d11b4975
estebanpablo89 <estebanpablo89@gmail.com>
2021-05-04 21:37:44 -0500

    provisioning AWS SQS message queue (mail queue)

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -3,19 +3,25 @@ import AWS from 'aws-sdk';
 const ses = new AWS.SES({ region: 'us-east-2' });
 
 async function sendMail(event, context) {
+  const record = event.Records[0];
+  console.log('record processing', record);
+
+  const email = JSON.parse(record.body);
+  const { subject, body, recipient } = email;
+
   const params = {
     Source: 'estebanpablo89@gmail.com',
     Destination: {
-      ToAddresses: ['estebanpablo89@gmail.com'],
+      ToAddresses: [recipient],
     },
     Message: {
       Body: {
         Text: {
-          Data: 'Hello from the world',
+          Data: body,
         },
       },
       Subject: {
-        Data: 'Test mail',
+        Data: subject,
       },
     },
   };
